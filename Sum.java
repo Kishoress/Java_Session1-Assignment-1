6ee913a6d0dfa168a5d2ab75a91f4302c91e3841
Kishoress <shrek.kishore@gmail.com>
2016-03-29 12:10:35 +0530

    Added files via upload

---
+++ Sum.java
@@ -0,0 +1,16 @@
+package Assign1;
+
+public class Sum {
+
+	public static void main(String[] args) {
+
+		int x=5;
+		float y= (float) 10.0;
+		System.out.println("Value of X: " + x);
+		System.out.println("Value of Y: " + y);
+		int z = (int) (x+y);
+		System.out.println("Sum of X and y = "+z);
+
+	}
+
+}
